ae04942896176d3b9b690b4fea3058be7db722d7
HASEUNGHEEE <ariul123@gmail.com>
2022-07-13 19:04:55 +0900

    재업로드-main_app.py 문제 해결

--- main_app.py
+++ main_app.py
@@ -1,104 +1,84 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-from pymongo import MongoClient
-import certifi
-import jwt
-import datetime
-import hashlib
-from werkzeug.utils import secure_filename
-from datetime import datetime, timedelta
-
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
-client = MongoClient('mongodb+srv://test:sparta@cluster0.mnffv.mongodb.net/Cluster0?retryWrites=true&w=majority',
-                     tlsCAFile=certifi.where())
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.wvkfw4y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = client.dbwebtoon
 
-SECRET_KEY = 'SPARTA'
+import random
 
 
 @app.route('/')
-def home():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        userinfo = db.t_user.find_one({"user_id": payload["id"]})
-        user_id = userinfo['user_id']
-        return render_template('index.html', user_id=user_id)
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
-@app.route('/register')
-def register_back():
-    msg = request.args.get("msg")
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        userinfo = db.t_user.find_one({"user_id": payload["id"]})
-        user_id = userinfo['user_id']
-        return render_template('index.html', user_id=user_id, msg="현재 로그인되어 있습니다.")
-    except jwt.ExpiredSignatureError:
-        return render_template('register.html', msg=msg)
-    except jwt.exceptions.DecodeError:
-        return render_template('register.html', msg=msg)
-
-@app.route('/login')
-def login():
-    msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
-
-@app.route('/sign_in', methods=['POST'])
-def sign_in():
-    # 로그인
-    username_receive = request.form['username_give']
-    password_receive = request.form['password_give']
-
-    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.t_user.find_one({'user_id': username_receive, 'user_pw': pw_hash})
-
-    # 토큰생성
-    if result is not None:
-        payload = {
-            'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
-    else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+def main():
+    return render_template('main.html')
 
 
-@app.route('/register/save', methods=['POST'])
-def sign_up():
 
-    username_receive = request.form['username_give']
-    email_receive = request.form['email_give']
-    password_receive = request.form['password_give']
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    doc = {
-        "user_id": username_receive,
-        "user_email": email_receive,
-        "user_pw": password_hash,
-    }
-    db.t_user.insert_one(doc)
-    return jsonify({'result': 'success'})
+@app.route('/webtoon', methods=['POST'])
+def posting():
+    # # name_receive = 토큰의 id 를 이용하여 db 에서 이름 가져오기
+    # token_receive = request.cookies.get('mytoken')
+    #     try:
+    #         # 토큰으로부터 payload를 불러옴
+    #         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    #         userinfo = db.user.find_one({"user_id": payload['id']})
+    #         # payload 내에 있는 user_id를 변수에 할당
+    #         user_id = userinfo['user_id']
 
 
-@app.route('/register/check_dup', methods=['POST'])
-def check_dup():
-    username_receive = request.form['username_give']
-    exists = bool(db.t_user.find_one({"user_id": username_receive}))
-    # print(value_receive, type_receive, exists)
-    return jsonify({'result': 'success', 'exists': exists})
+            url_receive = request.form['url_give']
+            star_receive = request.form['star_give']
+            comment_receive = request.form["comment_give"]
 
-<<<<<<< HEAD
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+            data = requests.get(url_receive, headers=headers)
+
+            soup = BeautifulSoup(data.text, "lxml")
+            image = soup.select_one('meta[property="og:image"]')['content']
+            name = soup.select_one('meta[property="og:title"]')['content']
+
+
+            detailInfo = soup.select_one('.comicinfo > .detail')
+            desc = detailInfo.select_one('p').text.replace('<br/>', '\n')
+            genre = detailInfo.select_one('.genre').text
+            writer = detailInfo.select_one('.wrt_nm').text[8:]
+
+
+            doc = {
+                # "user_id": user_info["username"],
+                "image": image,
+                "url": url_receive,
+                "comment": comment_receive,
+                "star": star_receive,
+                "name": name,
+                "desc": desc,
+                "genre": genre,
+                "writer": writer
+            }
+            db.t_webtoon.insert_one(doc)
+
+            return jsonify({"result": "success", 'msg': '포스팅 완료'})
+        # except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        #     return redirect(url_for("home"))
+
+
+@app.route("/webtoon", methods=['GET'])
+def listing():
+    webtoon_list = list(db.t_webtoon.find({}, {'_id': False}))
+    random_list = random.sample(webtoon_list, k=4)
+
     return jsonify({'webtoons': random_list})
-=======
->>>>>>> 5191674d81a8224fa9e09873fd6445f478deb682
+    # token_receive = request.cookies.get('mytoken')
+    # try:
+    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    #     # 포스팅 목록 받아오기
+    #     return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다."})
+    # except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+    #     return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
