7c8ea545cb27698f8ed4469be28a4e02421caa20
SoonWook <bravadosw@naver.com>
2022-07-13 20:36:43 +0900

    my_app.py

--- my_app.py
+++ my_app.py
@@ -3,9 +3,11 @@ app = Flask(__name__)
 
 import requests
 from bs4 import BeautifulSoup
+import certifi
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.clgy5.mongodb.net/cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.mnffv.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=certifi.where())
 db = client.dbwebtoon
 
 
@@ -20,17 +22,16 @@ def detail():
     return render_template('mypage.html', name=username)
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
-
 
 @app.route('/webtoon', methods=['POST'])
 def posting():
     # token_receive = request.cookies.get('mytoken')
     # try:
     #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-    #     user_info = db.users.find_one({"username": payload["id"]})
+    #     user_info = db.users.find_one({"user_id": payload["id"]})
+    #     user_id = userinfo['user_id']
+    #     user_id = userinfo['user_id']
+
         url_receive = request.form['url_give']
         star_receive = request.form['star_give']
         comment_receive = request.form["comment_give"]
@@ -40,15 +41,16 @@ def posting():
         data = requests.get(url_receive, headers=headers)
 
         soup = BeautifulSoup(data.text, 'html.parser')
-        image = soup.select_one('meta[property="og:image"]')['content']
-        name = soup.select_one('meta[property="og:title"]')['content']
-        desc = soup.select_one('meta[property="og:description"]')['content']
+        # image = soup.select_one('meta[property="og:image"]')['content']
+        # name = soup.select_one('meta[property="og:title"]')['content']
+        # desc = soup.select_one('meta[property="og:description"]')['content']
 
         doc = {
             "url": url_receive,
             "star": star_receive,
             "comment": comment_receive,
         }
+
         db.t_webtoon.insert_one(doc)
 
         return jsonify({"result": "success", 'msg': '포스팅 완료'})
@@ -67,11 +69,11 @@ def listing():
     # except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
     #     return redirect(url_for("home"))
 
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
-
 # @app.route("/dbwebtoon", methods=["GET"])
 # def dbwebtoon_get():
 #     dbwebtoon_list = list(db.dbwebtoon.find({}, {'_id': False}))
 #     return jsonify({'dbwebtoon': dbwebtoon_list})
 
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
